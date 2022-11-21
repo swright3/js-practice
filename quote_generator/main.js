00d2ccae1658c13b6e274606bd7c32f8086bd418
Sam <samwrighthwk@gmail.com>
2022-11-21 21:20:55 +0000

    Changed to async await fetch that checks response code

--- quote_generator/main.js
+++ quote_generator/main.js
@@ -1,11 +1,13 @@
-function fetchQuote() {
-    fetch('https://api.quotable.io/random')
-    .then(res => res.json())
-    .then(data => {
+async function fetchQuote() {
+    try {
+        const res = await fetch('https://api.quotable.io/random')
+        if (!res.ok) {throw `Response status code: ${res.status}`}
+        const data = await res.json()
         document.getElementById('quote').innerText = data.content
         document.getElementById('name').innerText = data.author.toUpperCase()
-    })
-    .catch(err => console.log(err))
+    } catch(err) {
+        console.log(err)
+    }
 }
 
 const button = document.getElementById('newQuote')
