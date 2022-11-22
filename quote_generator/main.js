d3d7b3a232e464ce8c6183e24fc3ac9343da664b
Sam <samwrighthwk@gmail.com>
2022-11-22 02:41:04 +0000

    Nearly got options menu to appear and disappear properly

--- quote_generator/main.js
+++ quote_generator/main.js
@@ -1,6 +1,7 @@
-async function fetchQuote() {
+async function fetchQuote(url) {
     try {
-        const res = await fetch('https://api.quotable.io/random')
+        console.log(url)
+        const res = await fetch(url)
         if (!res.ok) {throw `Response status code: ${res.status}`}
         const data = await res.json()
         document.getElementById('quote').innerText = data.content
@@ -15,7 +16,7 @@ async function fetchTags() {
         const res = await fetch('https://api.quotable.io/tags')
         if (!res.ok) {throw `Response status code: ${res.status}`}
         const data = await res.json()
-        const optionsForm = document.getElementById('optionsForm')
+        const outerTagContainer = document.querySelector('.outerTagContainer')
         for (const tag of data) {
             const tagContainer = document.createElement('div')
             tagContainer.classList.add('tagContainer')
@@ -30,7 +31,7 @@ async function fetchTags() {
             newTagLabel.innerText = tag.name
             newTagLabel.setAttribute('for', `${tag.name}Tag`)
             tagContainer.append(newTagInput, newTagLabel)
-            optionsForm.appendChild(tagContainer)
+            outerTagContainer.appendChild(tagContainer)
         }
     } catch(err) {
         console.log(err)
@@ -44,16 +45,44 @@ function logTags(tags) {
     }
 }
 
+function constructUrl() {
+    const allTags = Array.from(document.getElementsByName('tags'))
+    const selectedTags = allTags.filter(tag => tag.checked)
+    if (selectedTags.length > 0) {
+        const url = 'https://api.quotable.io/random?tags='
+        return selectedTags.reduce((newUrl,tag) => newUrl.concat(tag.value).concat('|'), url).slice(0,-1)
+    }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     const newQuoteButton = document.getElementById('newQuote')
-    newQuoteButton.addEventListener('click', fetchQuote)
+    newQuoteButton.addEventListener('click', () => fetchQuote('https://api.quotable.io/random'))
 
     const optionsForm = document.getElementById('optionsForm')
-
     fetchTags()
-
     optionsForm.addEventListener('submit', (event) => {
         event.preventDefault()
-        fetchQuote()
+        const apiUrl = constructUrl()
+        fetchQuote(apiUrl)
+    })
+
+    const options = document.getElementById('options')
+    options.classList.add('options')
+    options.classList.add('offpage')
+    options.classList.remove('invisible')
+    const optionsButton = document.getElementById('optionsButton')
+    optionsButton.addEventListener('click', () => {
+        const options = document.querySelector('.options')
+        options.matches('.offpage') 
+            ? options.classList.remove('offpage')
+            : options.classList.add('offpage')
+    })
+
+    const toggleTagButton = document.getElementById('toggleTags')
+    toggleTagButton.addEventListener('click', () => {
+        const tagContainer = document.querySelector('.outerTagContainer')
+        tagContainer.matches('.hidden') 
+            ? tagContainer.classList.remove('hidden')
+            : tagContainer.classList.add('hidden')
     })
 })
